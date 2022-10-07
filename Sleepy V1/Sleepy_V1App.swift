c9422a30862befa8eb1d97ab87f58b453274e510
Sanya Mathew <sanya.a.mathew@gmail.com>
2022-10-07 13:56:26 -0700

    Initial Commit

---
+++ Sleepy V1/Sleepy_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  Sleepy_V1App.swift
+//  Sleepy V1
+//
+//  Created by Bellevue on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Sleepy_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
